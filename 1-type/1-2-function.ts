e3a6cd7533af34cc08524d48d2604ee8aaf2361b
Yujin <jyj15938@gmail.com>
2023-07-15 16:07:50 +0900

    함수 타입 이용

--- 1-type/1-2-function.ts
+++ 1-type/1-2-function.ts
@@ -1,27 +1,51 @@
 {
-  function jsAdd(num1, num2) {
-    return num1 + num2;
+  // function jsAdd(num1, num2) {
+  //   return num1 + num2;
+  // }
+
+  // function tsAdd(num1: number, num2: number): number {
+  //   return num1 + num2;
+  // }
+
+  // function jsFetchNum(id) {
+  //   //code
+  //   //code
+  //   // code ..
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+
+  // function tsFetchNum(id: string): Promise<number> {
+  //   //code
+  //   //code
+  //   //code ..
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+
+  //Optinal parameter
+  function printName(firstName: string, lastName?: string) {
+    console.log(firstName);
+    console.log(lastName);
   }
 
-  function tsAdd(num1: number, num2: number): number {
-    return num1 + num2;
-  }
+  printName('안', '유진');
+  printName('김씨');
+  printName('이씨', undefined);
 
-  function jsFetchNum(id) {
-    //code
-    //code
-    // code ..
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
+  //default parameter
+  function defaultMessage(message: string = 'hello 기본메세지') {
+    console.log(message);
   }
+  defaultMessage();
 
-  function tsFetchNum(id: string): Promise<number> {
-    //code
-    //code
-    //code ..
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
+  //rest parameter
+  function addNumbers(...numbers: number[]): number {
+    return numbers.reduce((a, b) => a + b);
   }
+  console.log(addNumbers(4, 5));
+  console.log(addNumbers(2, 5, 10));
+  console.log(addNumbers(4004, 539));
 }
